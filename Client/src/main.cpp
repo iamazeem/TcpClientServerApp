4a5ab3f2936996b83465a7c4542a24e75731b63e
Azeem Sajid <azeem.sajid@gmail.com>
2021-04-03 09:16:08 +0500

    Remove excess whitespace.

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,13 +1,10 @@
 #include "common.hpp"
 #include "client.hpp"
 
-
 int main()
 {
     Client client{ SERVER_IP, SERVER_PORT };
     client.start();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
-
-
