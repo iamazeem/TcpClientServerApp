74cff431a6209470592f0aefff1e9fdb440da013
Azeem Sajid <azeem.sajid@gmail.com>
2023-08-19 19:20:42 +0500

    Remove manual flushing.

--- client/main.cpp
+++ client/main.cpp
@@ -6,7 +6,6 @@
 int main()
 {
     spdlog::default_logger()->set_pattern("%+");
-    spdlog::default_logger()->flush_on(spdlog::level::debug);
 
     client client{SERVER_IP, SERVER_PORT};
     client.start();
--- server/main.cpp
+++ server/main.cpp
@@ -6,7 +6,6 @@
 int main()
 {
     spdlog::default_logger()->set_pattern("%+");
-    spdlog::default_logger()->flush_on(spdlog::level::debug);
 
     server server{SERVER_IP, SERVER_PORT, SERVER_THREADS};
     server.start();
